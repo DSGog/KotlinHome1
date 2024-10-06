bec769ad915fce5f53eb9f994ae78fb92bd05399
DSGog <dmitriy.amsec@gmail.com>
2024-10-06 15:08:33 +0400

    Первое домашнее задание

--- src/Main.kt
+++ src/Main.kt
@@ -1,61 +1,48 @@
 fun main() {
-    val phoneBook = mutableMapOf<String, MutableMap<String, String>>()
+    var lastPerson: Person? = null
+
+    println("Введите 'help' для получения списка доступных команд.")
 
     while (true) {
-        print("> ")
-        val input = readLine()?.trim() ?: continue
-        val parts = input.split(" ")
+        val command = readCommand()
+        println(command)
 
-        when (parts[0].lowercase()) {
-            "exit" -> {
+        if (!command.isValid()) {
+            println("Неверный формат команды. Введите 'help' для получения списка команд.")
+            continue
+        }
+
+        when (command) {
+            is AddCommand -> {
+                if (command.type == "phone") {
+                    lastPerson = Person(command.name, command.value, null)
+                    println("Телефон для ${command.name} добавлен: ${command.value}")
+                } else if (command.type == "email") {
+                    lastPerson = Person(command.name, lastPerson?.phone, command.value)
+                    println("Email для ${command.name} добавлен: ${command.value}")
+                }
+            }
+
+            is HelpCommand -> {
+                println("Доступные команды:")
+                println("1. add <Имя> <phone или email> <номер или значение>. Например: вводимое значение будет выглядеть следующим образом > add Andrey phone +1234567890")
+                println("2. exit - Выйти из программы")
+                println("3. show - Показать последнюю запись")
+                println("4. help - Показать доступные команды")
+            }
+
+            is ExitCommand -> {
                 println("Программа завершена.")
                 break
             }
-            "help" -> {
-                println("Доступные команды:")
-                println("1. add <Имя> phone <Номер телефона> - Добавить телефон для имени")
-                println("2. add <Имя> email <Адрес электронной почты> - Добавить email для имени")
-                println("3. exit - Выйти из программы")
-                println("4. help - Показать доступные команды")
-            }
-            "add" -> {
-                if (parts.size >= 4) {
-                    val name = parts[1]
-                    val type = parts[2].lowercase()
-                    val value = parts.subList(3, parts.size).joinToString(" ")
 
-                    when (type) {
-                        "phone" -> {
-                            if (isValidPhoneNumber(value)) {
-                                phoneBook.getOrPut(name) { mutableMapOf() }["phone"] = value
-                                println("Телефон для $name добавлен: $value")
-                            } else {
-                                println("Ошибка: Неверный формат номера телефона.")
-                            }
-                        }
-                        "email" -> {
-                            if (isValidEmail(value)) {
-                                phoneBook.getOrPut(name) { mutableMapOf() }["email"] = value
-                                println("Email для $name добавлен: $value")
-                            } else {
-                                println("Ошибка: Неверный формат адреса электронной почты.")
-                            }
-                        }
-                        else -> println("Неверный формат команды. Введите 'help' для получения списка команд.")
-                    }
+            is ShowCommand -> {
+                if (lastPerson == null) {
+                    println("Not initialized")
                 } else {
-                    println("Неверный формат команды. Введите 'help' для получения списка команд.")
+                    println("Последняя запись: $lastPerson")
                 }
             }
-            else -> println("Неизвестная команда. Введите 'help' для получения списка команд.")
         }
     }
-}
-
-fun isValidPhoneNumber(phoneNumber: String): Boolean {
-    return phoneNumber.matches(Regex("""^\+\d+$"""))
-}
-
-fun isValidEmail(email: String): Boolean {
-    return email.matches(Regex("""^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$"""))
 }
